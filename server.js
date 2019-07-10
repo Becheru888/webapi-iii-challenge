b922bab6d06c2dfbc000c35a4b3ac28707013e54
Becheru888 <becheru93@gmail.com>
2019-07-10 21:14:11 +0100

    routers imported

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const express = 'express';
-
+const express = require('express');
 const server = express();
+const userRouter = require('./posts/postRouter');
+const postRouter = require('./users/userRouter');
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
